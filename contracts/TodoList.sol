26ad25f9372cb2260cea1364d413965a3fc3fafc
callezenwaka <callisezenwaka@gmail.com>
2022-02-16 13:37:53 +0100

    Add test

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,7 +1,25 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.5.0 <0.9.0;
+// pragma solidity >=0.5.0 <0.9.0;
+pragma solidity >=0.7.0 <0.9.0;
 
 contract TodoList {
   uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  constructor() {
+    createTask("Initial task");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
 }
